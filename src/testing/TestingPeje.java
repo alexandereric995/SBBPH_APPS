8b425b544e2193446cac360b28ef68b78cc91e77
Mohd Faizal Hassan <mofaizalhassan@gmail.com>
2020-06-17 17:07:19 +0800

    update entities/qtr/KuaTawaran.java

--- src/testing/TestingPeje.java
+++ src/testing/TestingPeje.java
@@ -31,6 +31,8 @@ import bph.entities.kewangan.KewDeposit;
 import bph.entities.kewangan.KewInvois;
 import bph.entities.kewangan.KewResitSenaraiInvois;
 import bph.entities.kod.BankFPX;
+import bph.entities.qtr.KuaAgihan;
+import bph.entities.qtr.KuaAkaun;
 import bph.integrasi.fpx.FPXPkiImplementation;
 
 /**
@@ -48,8 +50,7 @@ public class TestingPeje {
 	public static void main(String[] args) {
 		System.out.println("START JOB ON : " + new Date());
 		
-//		doJob();
-		updateStatusFPXBankList(db);
+		doJob();
 		
 		System.out.println("FINISH JOB ON : " + new Date());
 	}
@@ -60,43 +61,15 @@ public class TestingPeje {
 			db = new DbPersistence();
 			lebahDb = new Db();
 			Statement stmt = lebahDb.getStatement();	
-			int z = 1;
-			for (int i = 2014; i < 2020; i++) {
-				for (int y = 1; y <= 12; y++) {
-					db.begin();
-					String sql = "SELECT id FROM kew_bayaran_resit WHERE id_pembayar != no_pengenalan_pembayar AND flag_jenis_bayaran != 'ONLINE'"
-							+ " and YEAR(tarikh_resit) = '" + i + "' and MONTH(tarikh_resit) = '" + y + "'";
-					ResultSet rs = stmt.executeQuery(sql);
-					while (rs.next()) {
-						KewBayaranResit resit = db.find(KewBayaranResit.class, rs.getString("id"));
-						if (resit != null) {
-							List<KewResitSenaraiInvois> listRSI = db.list("select x from KewResitSenaraiInvois x where x.resit.id = '" + resit.getId() + "'");
-							if (listRSI.size() > 0) {
-								for (KewResitSenaraiInvois rsi : listRSI) {
-									if (rsi.getDeposit() != null) {
-										KewDeposit deposit = rsi.getDeposit();
-										if (deposit.getPendeposit() != null && !deposit.getPendeposit().getId().equalsIgnoreCase("FAIZAL")) {
-											resit.setPembayar(deposit.getPendeposit());
-										}
-//										System.out.println("DEPOSIT : " + resit.getNoResit() + " - " + resit.getPembayar().getId() + " - " + deposit.getPendeposit().getId());
-									}
-									
-									if (rsi.getInvois() != null) {
-										KewInvois invois = rsi.getInvois();
-										if (invois.getPembayar() != null && !invois.getPembayar().getId().equalsIgnoreCase("FAIZAL")) {
-											resit.setPembayar(invois.getPembayar());
-										}
-//										System.out.println("INVOIS : " + resit.getNoResit() + " - " + resit.getPembayar().getId() + " - " + invois.getUserPendaftar().getId());
-									}
-								}
-							}
-							System.out.println(z);
-							z++;
-						}
-					}
-					db.commit();					
+			int i = 0;
+			List<KuaAkaun> listAkaun = db.list("select x from KuaAkaun x where x.kodHasil.id = '72310' and x.flagBayar = 'Y' and x.penghuni is null");
+			for (KuaAkaun akauan : listAkaun) {
+				KuaAgihan agihan = (KuaAgihan) db.get("select x from KuaAgihan x where x.pemohon.id = '" + akauan.getPembayar().getId() + "'");
+				if (agihan != null && agihan.getKuarters() != null) {
+					i++;
 				}
-			}			
+			}
+			System.out.println(i);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -104,95 +77,4 @@ public class TestingPeje {
 		}
 	}
 	
-	private static void updateStatusFPXBankList(DbPersistence db3) {
-		try {	
-			db3 = new DbPersistence();
-			db3.begin();
-			List<BankFPX> listBankFPX = db3.list("select x from BankFPX x where x.isActive = 'Y'");
-			for (BankFPX bankFPX : listBankFPX) {
-				bankFPX.setIsOnline("T");
-			}
-			db3.commit();
-			
-			String fpx_msgType = "BE";
-			String fpx_msgToken = "01";
-			String fpx_sellerExId = ResourceBundle.getBundle("dbconnection").getString("FPX_SELLER_EX_ID");
-			String fpx_version = "7.0";
-
-			HashMap<String, String> respMap = new HashMap<String, String>();
-
-			String chkSumStr = fpx_msgToken + "|" + fpx_msgType + "|"
-					+ fpx_sellerExId + "|" + fpx_version;
-
-			String final_checkSum = FPXPkiImplementation.signData(
-					ResourceBundle.getBundle("dbconnection").getString("FPX_KEY_PATH"), chkSumStr, "SHA1withRSA");
-			StringBuilder postDataStrBuilder = new StringBuilder();
-			postDataStrBuilder.append("fpx_msgType="
-					+ URLEncoder.encode(fpx_msgType, "UTF-8"));
-			postDataStrBuilder.append("&fpx_msgToken="
-					+ URLEncoder.encode(fpx_msgToken, "UTF-8"));
-			postDataStrBuilder.append("&fpx_sellerExId="
-					+ URLEncoder.encode(fpx_sellerExId, "UTF-8"));
-			postDataStrBuilder.append("&fpx_version="
-					+ URLEncoder.encode(fpx_version, "UTF-8"));
-			postDataStrBuilder.append("&fpx_checkSum="
-					+ URLEncoder.encode(final_checkSum, "UTF-8"));
-
-			URLConnection conn = (HttpsURLConnection) new URL(
-					"https://mepsfpx.com.my/FPXMain/RetrieveBankList")
-					.openConnection();
-
-			conn.setDoOutput(true);
-			BufferedWriter outputWriter = new BufferedWriter(
-					new OutputStreamWriter(conn.getOutputStream()));
-			outputWriter.write(postDataStrBuilder.toString(), 0, postDataStrBuilder
-					.toString().length());
-
-			outputWriter.flush();
-			outputWriter.close();
-			
-			BufferedReader inputReader = new BufferedReader(new InputStreamReader(
-					conn.getInputStream()));
-			String strResponse = inputReader.readLine();
-			inputReader.close();
-			strResponse = strResponse.trim();
-			if (strResponse == null || strResponse.equals("ERROR")) {
-				System.out.println("An error occured!..Response[" + strResponse
-						+ "]");
-				return;
-			} else {
-				StringTokenizer strToknzr = new StringTokenizer(strResponse, "&");
-				while (strToknzr.hasMoreElements()) {
-					String temp = strToknzr.nextToken();
-					if (temp.contains("=")) {
-						String nvp[] = temp.split("=");
-						String name = nvp[0];
-						String value = "";
-						if (nvp.length == 2)
-							value = URLDecoder.decode(nvp[1], "UTF-8");
-						respMap.put(name, value);					
-					} else {
-						System.out.println("Parsing Error!" + temp);
-					}
-				}				
-				
-				db3.begin();
-				String[] parts = respMap.get("fpx_bankList").toString().split(",");
-				for (String r:parts) {
-					String bankCode = StringUtils.substringBefore(r, "~").trim();
-					String isActive = StringUtils.substringAfter(r, "~").trim();
-					
-					BankFPX bankFPX = (BankFPX) db3.get("select x from BankFPX x where x.isActive = 'Y' and x.code = '" + bankCode + "'");
-					if (bankFPX != null) {
-						if ("A".equals(isActive)) {
-							bankFPX.setIsOnline("Y");
-						}
-					}
-				}
-				db3.commit();
-			}
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
 }
